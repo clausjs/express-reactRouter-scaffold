1a14991eeeeec508e2d41425e47a840859637cb9
Joe Claus <joseph.s.claus@gmail.com>
2025-02-10 15:30:59 -0500

    v2 changes. Add typescript, example pages, show router usage

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   watch: false, 
   mode: 'development', 
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'build')
   },
   module: {
     rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
       {
         test: [/\.js$/, /\.jsx$/],
         exclude: /node_modules/,
@@ -21,6 +26,10 @@ module.exports = {
         use:['style-loader','css-loader', 'sass-loader']
       }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.tsx', '.ts', '.html', '.scss'],
+    modules: ['node_modules'],
   },
   plugins: [
     new HtmlWebpackPlugin({
